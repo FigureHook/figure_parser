ff4c7ea9ddcf78f02048b080bde89e5f7e3efe02
Elton Chou <plscd748@gmail.com>
2022-08-22 03:43:23 +0800

    add native parser to general product factory

--- figure_parser/factories.py
+++ figure_parser/factories.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 from figure_parser.core.factory.base import GenericProductFactory
 
-from .parsers import AlterProductParser, GSCProductParser
+from .parsers import AlterProductParser, GSCProductParser, NativeProductParser
 from .pipes import (normalize_general_fields, normalize_worker_fields,
                     sort_releases)
 
@@ -17,6 +17,7 @@ class GeneralBs4ProductFactory(Bs4ProductFactory):
         factory = cls()
         factory.register_parser('goodsmile.info', GSCProductParser)
         factory.register_parser('alter-web.jp', AlterProductParser)
+        factory.register_parser('native-web.jp', NativeProductParser)
         factory.add_pipes(
             (normalize_general_fields, 1),
             (normalize_worker_fields, 2),
