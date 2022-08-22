934d19d6079def6ed9407dc2bfa396bb3e568086
Elton Chou <plscd748@gmail.com>
2022-08-23 05:57:40 +0800

    add amakuni product parser to factory

--- figure_parser/factories.py
+++ figure_parser/factories.py
@@ -2,7 +2,8 @@ from bs4 import BeautifulSoup
 
 from figure_parser.core.factory.base import GenericProductFactory
 
-from .parsers import AlterProductParser, GSCProductParser, NativeProductParser
+from .parsers import (AlterProductParser, AmakuniProductParser,
+                      GSCProductParser, NativeProductParser)
 from .pipes import (normalize_general_fields, normalize_worker_fields,
                     sort_releases)
 
@@ -15,8 +16,9 @@ class GeneralBs4ProductFactory(Bs4ProductFactory):
     @classmethod
     def create_factory(cls):
         factory = cls()
-        factory.register_parser('goodsmile.info', GSCProductParser)
         factory.register_parser('alter-web.jp', AlterProductParser)
+        factory.register_parser('amakuni.info', AmakuniProductParser)
+        factory.register_parser('goodsmile.info', GSCProductParser)
         factory.register_parser('native-web.jp', NativeProductParser)
         factory.add_pipes(
             (normalize_general_fields, 1),
