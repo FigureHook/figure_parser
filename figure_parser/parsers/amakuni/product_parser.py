c18793089bfef06ce079c643906a05903f5c623c
Elton Chou <plscd748@gmail.com>
2022-08-23 01:05:09 +0800

    add amakuni formal parser

--- figure_parser/parsers/amakuni/product_parser.py
+++ figure_parser/parsers/amakuni/product_parser.py
@@ -97,6 +97,44 @@ def parse_workers(workers_text: str) -> List[str]:
     return workers_text.split("、")
 
 
+def _parse_order_period(text: str) -> OrderPeriod:
+    pattern = r"●受注期間／(?P<start>(\d+)年(\d+)月(\d+)日)\uff5e(?P<end>((\d+)年)?(\d+)月(\d+)日)"
+    date_format = "%Y年%m月%d日"
+    matched = re.search(pattern, text)
+    if matched:
+        start_str = matched.group('start')
+        end_str = matched.group('end')
+        start_date = datetime.strptime(start_str, date_format)
+        try:
+            end_date = datetime.strptime(end_str, date_format)
+        except ValueError:
+            end_date = datetime.strptime(f"{start_date.year}年" + end_str, date_format)
+        end_date = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
+        return OrderPeriod(start=start_date, end=end_date)
+    return OrderPeriod()
+
+
+def _parse_prices(text: str) -> List[Tuple[int, bool]]:
+    prices = []
+    price_pattern = r"●価格(.+?)税(抜|込)"
+    matched = re.search(price_pattern, text)
+    if matched:
+        price = price_parse(matched.group(0))
+        tax_including = "税込" in matched.group(0)
+        prices.append((price, tax_including))
+    else:
+        prices.append((None, False))
+    return prices
+
+
+def _parse_release_dates(text: str) -> List[date]:
+    date_pattern = r"●(発送予定|発売|発送)\uff0f(\d+)年(\d+)月"
+    date_matched = re.search(date_pattern, text)
+    assert date_matched
+    release_date = date(int(date_matched.group(2)), int(date_matched.group(3)), 1)
+    return [release_date]
+
+
 class AmakuniLegacyProductParser(AbstractBs4ProductParser):
     _info: LegacyProductInfo
     _name: Optional[str]
@@ -149,25 +187,10 @@ class AmakuniLegacyProductParser(AbstractBs4ProductParser):
         return "フィギュア"
 
     def parse_prices(self) -> List[Tuple[int, bool]]:
-        prices = []
-        price_pattern = r"●価格(.+?)税(抜|込)"
-        matched = re.search(price_pattern, self._info.info_text)
-        if matched:
-            price = price_parse(matched.group(0))
-            tax_including = "税込" in matched.group(0)
-            prices.append((price, tax_including))
-        else:
-            prices.append((None, False))
-        return prices
+        return _parse_prices(self._info.info_text)
 
     def parse_release_dates(self) -> List[date]:
-        # pattern = r"●発売／(.+?)●"
-        # matched = re.search(pattern, self._info.info_text)
-        date_pattern = r"●(発送予定|発売|発送)\uff0f(\d+)年(\d+)月"
-        date_matched = re.search(date_pattern, self._info.info_text)
-        assert date_matched
-        release_date = date(int(date_matched.group(2)), int(date_matched.group(3)), 1)
-        return [release_date]
+        return _parse_release_dates(self._info.info_text)
 
     @cache
     def parse_series(self) -> Optional[str]:
@@ -243,68 +266,118 @@ class AmakuniLegacyProductParser(AbstractBs4ProductParser):
         return None
 
     def parse_order_period(self) -> OrderPeriod:
-        pattern = r"●受注期間／(?P<start>(\d+)年(\d+)月(\d+)日)\uff5e(?P<end>((\d+)年)?(\d+)月(\d+)日)"
-        date_format = "%Y年%m月%d日"
-        matched = re.search(pattern, self._info.info_text)
-        if matched:
-            start_str = matched.group('start')
-            end_str = matched.group('end')
-            start_date = datetime.strptime(start_str, date_format)
-            try:
-                end_date = datetime.strptime(end_str, date_format)
-            except ValueError:
-                end_date = datetime.strptime(f"{start_date.year}年" + end_str, date_format)
-            end_date = datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59)
-            return OrderPeriod(start=start_date, end=end_date)
-        return OrderPeriod()
+        return _parse_order_period(self._info.info_text)
 
     def parse_JAN(self) -> Optional[str]:
         return None
 
 
 class AmakuniFormalProductParser(AbstractBs4ProductParser):
-    def __init__(self, source: BeautifulSoup):
+    _detail_text: str
+    _source_url: str
+
+    def __init__(self, url: str, source: BeautifulSoup, detail_text: str):
+        self._detail_text = detail_text
+        self._source_url = url
         super().__init__(source)
 
     @classmethod
     def create_parser(cls, url: str, source: BeautifulSoup):
-        return cls(source=source)
+        detail_ele = source.select_one(".product_details")
+        if not detail_ele:
+            raise ParserInitializationFailed
+        detail_text = detail_ele.text.strip()
+        return cls(url=url, source=source, detail_text=detail_text)
 
-    def parse_name(self) -> str: ...
+    def parse_name(self) -> str:
+        name_ele = self.source.select_one(".product_name > span:nth-last-child(1)")
+        assert name_ele
+        name = name_ele.text.strip()
+        return name
 
-    def parse_adult(self) -> bool: ...
+    def parse_adult(self) -> bool:
+        return False
 
-    def parse_manufacturer(self) -> str: ...
+    def parse_manufacturer(self) -> str:
+        return "AMAKUNI"
 
-    def parse_category(self) -> str: ...
+    def parse_category(self) -> str:
+        return "フィギュア"
 
-    def parse_prices(self) -> List[Tuple[int, bool]]: ...
+    def parse_prices(self) -> List[Tuple[int, bool]]:
+        return _parse_prices(self._detail_text)
 
-    def parse_release_dates(self) -> List[date]: ...
+    def parse_release_dates(self) -> List[date]:
+        return _parse_release_dates(self._detail_text)
 
-    def parse_series(self) -> Optional[str]: ...
+    def parse_series(self) -> Optional[str]:
+        series_ele = self.source.select_one(".product_name > span:nth-child(1)")
+        if series_ele:
+            series = series_ele.text.strip()
+            return series
+        return None
 
-    def parse_paintworks(self) -> List[str]: ...
+    def parse_paintworks(self) -> List[str]:
+        pattern = r"彩色見本製作／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        return parse_workers(matched.group(1).strip()) if matched else []
 
-    def parse_sculptors(self) -> List[str]: ...
+    def parse_sculptors(self) -> List[str]:
+        pattern = r"●原型製作／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        return parse_workers(matched.group(1).strip()) if matched else []
 
-    def parse_scale(self) -> Optional[int]: ...
+    def parse_scale(self) -> Optional[int]:
+        pattern = r"●仕様／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        if matched:
+            return scale_parse(matched.group(1))
+        return None
 
-    def parse_size(self) -> Optional[int]: ...
+    def parse_size(self) -> Optional[int]:
+        pattern = r"●仕様／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        if matched:
+            return size_parse(matched.group(1))
+        return None
 
-    def parse_copyright(self) -> Optional[str]: ...
+    def parse_copyright(self) -> Optional[str]:
+        copyright_ele = self.source.select_one(".copyright")
+        if copyright_ele:
+            return copyright_ele.text.strip()
+        return None
 
-    def parse_releaser(self) -> Optional[str]: ...
+    def parse_releaser(self) -> Optional[str]:
+        pattern = r"●発売元／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        if matched:
+            return matched.group(1)
+        return "ホビージャパン"
 
-    def parse_distributer(self) -> Optional[str]: ...
+    def parse_distributer(self) -> Optional[str]:
+        pattern = r"●販売元／(.+)"
+        matched = re.search(pattern, self._detail_text)
+        if matched:
+            return matched.group(1)
+        return "ホビージャパン"
 
-    def parse_rerelease(self) -> bool: ...
+    def parse_rerelease(self) -> bool:
+        return False
 
-    def parse_images(self) -> List[str]: ...
+    def parse_images(self) -> List[str]:
+        images: List[str] = []
+        image_anchors = self.source.select("[rel='lightbox[01]']")
+        for a in image_anchors:
+            image_src = a.get("href")
+            if type(image_src) is str:
+                image_src = urljoin(self._source_url, image_src)
+                images.append(image_src)
+        return images
 
     def parse_thumbnail(self) -> Optional[str]: ...
 
-    def parse_order_period(self) -> OrderPeriod: ...
+    def parse_order_period(self) -> OrderPeriod:
+        return _parse_order_period(self._detail_text)
 
     def parse_JAN(self) -> Optional[str]: ...
 
